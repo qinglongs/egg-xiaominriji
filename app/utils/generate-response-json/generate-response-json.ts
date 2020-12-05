212f28bd4ed4b099d420a5c5440d07e289e8208c
lqllm <924229679@qq.com>
2020-12-05 14:37:17 +0800

    feat: ts接口命名调整

--- app/utils/generate-response-json/generate-response-json.ts
+++ app/utils/generate-response-json/generate-response-json.ts
@@ -1,12 +1,12 @@
 
-interface Generate<T> { code: number, data: T | any, msg: string }
+interface Generater<T> { code: number, data: T | any, msg: string }
 
 /**
- * @function 生成返回的json
+ * @function 生成成功信息
  * @param data 返回的数据
  * @param msg 成功描述
  */
-export function generateSuccess<T>(data?: T, msg = 'success'): Generate<T> {
+export function generateSuccess<T>(data?: T, msg = 'success'): Generater<T> {
   return { code: 0, data: data || null, msg };
 }
 
@@ -15,7 +15,7 @@ export function generateSuccess<T>(data?: T, msg = 'success'): Generate<T> {
  * @param data 返回的错误数据
  * @param msg 错误描述
  */
-export function generateError<T>(data?: T, msg = '出错了，请联系管理员', code = 1000): Generate<T> {
+export function generateError<T>(data?: T, msg = '出错了，请联系管理员', code = 1000): Generater<T> {
   return { code, data: data || null, msg };
 }
 
