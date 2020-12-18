a2c4448406f72ebd559b05fcf09e3bcbcefa29ba
lqllm <924229679@qq.com>
2020-12-18 18:02:31 +0800

    feat: 调整注释

--- app/utils/types/log-types.ts
+++ app/utils/types/log-types.ts
@@ -1,10 +1,14 @@
-// 日志分页参数
+/**
+ * 日志分页参数
+ */
 export type GetLogListParams = {
   page: number;
   size: number;
 };
 
-// 日志详情数据
+/**
+ * 日志详情数据
+ */
 export type LogData = {
   iamge: string;
   title: string;
@@ -12,7 +16,9 @@ export type LogData = {
   id: number;
 };
 
-// 获取日志分页数据service返回的数据
+/**
+ * 获取日志分页数据service返回的数据
+ */
 export type GetLogListRetrun<T> = {
   list: T[];
   total: number;
